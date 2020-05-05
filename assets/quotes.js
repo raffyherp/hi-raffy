7279083b684ee362615756beec224e77af2a1a66
raffyherp <63196437+raffyherp@users.noreply.github.com>
2020-05-05 21:13:47 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,7 +1,7 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "raffyherp", quotes: "Whatever happens, keep breathing" },
+    { author: "raffyherp", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
     { author: "raffyherp", quotes: "Hey! I wuv chuuuu! UwU" },
     { author: "raffyherp", quotes: "I love you." },
     { author: "raffyherp", quotes: "I may not with you everyday, but I love you everyday." },
